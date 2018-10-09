cdcbc1f9a0cd9ee882cf9d8a324d081f982d27d7
maboglia <mauro.bogliaccino@gmail.com>
2018-10-09 18:16:37 +0200

    upl

--- app/pdo/connessione.php
+++ app/pdo/connessione.php
@@ -6,15 +6,7 @@ try {
 } catch (PDOException $eccezione) {
     echo $eccezione->getMessage();
 } finally {
-    
+    echo 'finally: questo codice viene eseguito in ogni caso';
 }
 
-echo 'ciao';
-
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+echo 'sei connesso';
